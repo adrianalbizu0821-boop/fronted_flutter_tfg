be404dddffe112eebb9054bd71dd9819d4bb16cf
Adrian Albizu <adrianalbizu0821@gmail.com>
2026-02-03 09:18:29 +0100

    Se hace arquitectura del proyecto y estructura base

---
+++ lib/features/news/presentation/pages/news_page.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class NewsPage extends StatelessWidget {
+  const NewsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Noticias'),
+      ),
+      body: const Center(
+        child: Text(
+          'Feed de noticias',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
